acf1bd6c0fba96703ec4a165da290cd9299cd17b
kenolion <veikeith@yahoo.com>
2016-06-09 22:59:06 +0800

    V0.2 ADDED  Simeple ONE SIDED BATTLE SYSTEM

--- Textbase RPG/Textbase RPG/main.cpp
+++ Textbase RPG/Textbase RPG/main.cpp
@@ -1,24 +1,35 @@
+
 #include <iostream>
 using namespace std;
 
-class character {
+class Character {
 	public:
 	char name[15];
 	int job; //1=Swordsman,2= Archer, 3= Deprived(not an actual class just decided to put it there)
-
-
+	int hp;
+	int minAtk;
+	int maxAtk;
+	int damageDealt;
 };
+
+
 
 int main() {
 	int choice; //Temporary variable to store choices of player.
-	character player; // Players information
+	Character player; // Players information
+	Character monster;
+	/////////////////// TEMPORARY DECLARATION AREA WILL BE REMOVED LATER FOR TESTING PURPOSES
+	monster.hp = 100; // Lets assume its 10 for now. going to remove this line of code later
+	player.maxAtk = 10;
+	player.minAtk = 1;
 
-	cout << "Please Enter your name: "; 
+	///////
+	cout << "Please Enter your name: ";
 	cin >> player.name;  //Gets player name
-	cout << "Master " << player.name <<". What job do you want to choose next?"<< endl;
+	cout << "Master " << player.name << ". What job do you want to choose next?" << endl;
 	cout << "1.Swordsman" << endl << "2.Archer" << endl << "3.Deprived" << endl;
 	cin >> player.job;
-	cout << "Character Info" << endl;
+	cout << "Character Info" << endl; //Displays character Info
 	cout << "Name : " << player.name << endl;
 	cout << "Class : ";
 	switch (player.job)
@@ -33,8 +44,19 @@ int main() {
 		cout << "Deprived";
 		break;
 	}
-	cout << endl;
-
+	//////// Simeple ONE SIDED BATTLE SYSTEM   /////////
+	cout << endl << "You have encountered a (monsters name)!" << endl;
+	cout << "Monster Hp :" << monster.hp << endl;
+	do {
+		cout << "What would you do?" << endl;
+		cout << "1.Attack" << endl << "2.Run" << endl;
+		cin >> choice;
+		if (choice == 1) {
+			player.damageDealt = rand() % player.maxAtk + player.minAtk; // CALCULATING THE ATTACK RANGE
+			monster.hp -= player.damageDealt; //MINUSING THE MONSTERS HP
+			cout << "You did " << player.damageDealt << " damage." << endl;
+			cout << "The monster now has " << monster.hp << "hp" << endl;
+		}
+	} while (monster.hp >= 0);
 	system("pause");
 }
-
