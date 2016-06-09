9b971619d36c38b4bcefab23d42cb7d706966f03
kenolion <veikeith@yahoo.com>
2016-06-09 15:42:39 +0800

    THIS IS WHERE IT BEGINS

---
+++ Programming assignment/Programming assignment/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "HI BEN ZER THIS IS WHERE WE DO OUR PROGRAMMMING ASS";
+	system("pause");
+}
---
+++ Textbase RPG/Textbase RPG/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "HI ZER BEN THIS IS WHERE IT STARTS AND ENDS";
+	system("pause");
+}
