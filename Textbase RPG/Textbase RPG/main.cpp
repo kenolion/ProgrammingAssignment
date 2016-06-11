b39c5087f908120134ea372376f6456bc6b04eab
kenolion <veikeith@yahoo.com>
2016-06-11 17:50:28 +0800

    V0.7 -Added Menu System

--- Textbase RPG/Textbase RPG/main.cpp
+++ Textbase RPG/Textbase RPG/main.cpp
@@ -8,8 +8,8 @@ Date : 10/6/2016
 #include <time.h> // to use the rand() function
 #include <windows.h>// to use the function sleep
 #include <string> //STRING DATA TYPE?!
-int BattleSystem();
-int DaySystem(int choice);
+int battleSystem();
+int daySystem(int choice);
 using namespace std;
 
 
@@ -21,6 +21,10 @@ class Character {
 	int minAtk;
 	int maxAtk;
 	int damageDealt;
+	int str;
+	int energy;
+	int intell;
+	int weaboo;
 };
 
 class Time {
@@ -38,12 +42,82 @@ class Time {
 	}
 }day;
 int choice; ////Temporary variable to store choices of player.
+class Menu {
+	public:
+	// ALL List of items in a menu will be here
+	string mainMenuItems[3] = { "Start Game", "Options", "Exit" };
+	string optionsMenuItems[4] = { "Zer","Keith","Ben","Test" };
+	int menuSystem(int itemsMenu,string menu[25]) {
+		
+		int pointer = 0;
+
+		while (true)
+		{
+			system("cls");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // SETS TEXT COLOR TO WHITE
+			cout << "Main Menu\n\n";
+
+			for (int i = 0; i < itemsMenu; ++i) {
+				if (i == pointer) {
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11); //sets text color to cyan
+					cout << menu[i] << endl;
+				}
+				else {
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+					cout << menu[i] << endl;
+				}
+			}
+			while (true) {
+				if (GetAsyncKeyState(VK_UP) != 0) { // Gets the up arrow key and each time its released it will give a random number that wont be 0
+					pointer -= 1;
+					if (pointer == -1) { // Basically brings the pointer to the bottom if the user cursor reaches the toppest
+						pointer = itemsMenu-1; //itemsmenu is placed there because different menu has different amount of items
+					}
+					break;
+				}
+				else if (GetAsyncKeyState(VK_DOWN) != 0) {
+					pointer += 1;
+					if (pointer == itemsMenu) { //Same thing but this time it is softcoded because the amount of items in menu may vary
+						pointer = 0;			//It brings the cursor back to the top when it reaches its end at the bottom
+					}
+					break;
+				}
+				else if (GetAsyncKeyState(VK_RETURN) != 0) {
+					return pointer;
+				}
+			}
+
+			Sleep(150);
+		}
+	}
+	int mainMenu(int pointer){
+		switch (pointer) {
+		case 0: {
+			cout << "Starting game...\n";
+			break;
+		}
+		case 1: {
+			cout << "This is the options\n";
+			break;
+		case 2:
+			exit(0);
+		}
+
+		}
+
+	}
+}menu;
+
 
 
 int main() {
 	Character player;
 	day.hour = 24;
-	
+	//Menu system start from here
+	int tempPointer;
+	tempPointer = menu.menuSystem(3,menu.mainMenuItems); // This calls the menu system function (x,y) X represents the number of list in the menu and Y represents the Items to be displayed
+	menu.mainMenu(tempPointer); // This function accepts the pointer value from above and decides on what to do next
+	///////
 	cout << "Please Enter your name: ";
 	cin >> player.name;  //Gets player name
 	cout << "Master " << player.name << ". What job do you want to choose next?" << endl;
@@ -93,14 +167,14 @@ int main() {
 		}
 		cout<< endl << "2.Add 1 hour(Temp function to test day/night)";
 		cin >> choice;
-		DaySystem(choice);
+		daySystem(choice);
 		
 		
 	}
 	system("pause");
 }
 
-int BattleSystem() {
+int battleSystem() {
 	Character player; // Players information
 	Character monster;
 	/////////////////// TEMPORARY DECLARATION AREA WILL BE REMOVED LATER FOR TESTING PURPOSES
@@ -136,7 +210,7 @@ int BattleSystem() {
 	return 0;
 }
 
-int DaySystem(int choice) {
+int daySystem(int choice) {
 	/*
 	if (choice == 1) {
 		cout << "Time now is " << day.hour;    // This section tells the player the time if they use check time
@@ -155,3 +229,4 @@ int DaySystem(int choice) {
 	}
 	return 0;
 }
+
