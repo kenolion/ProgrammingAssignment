1c8b2a120b84e171c92a53ec0105b41aa71017d1
kenolion <veikeith@yahoo.com>
2016-07-09 00:03:56 +0800

    test

--- Textbase RPG/Textbase RPG/Activities.cpp
+++ Textbase RPG/Textbase RPG/Activities.cpp
@@ -15,6 +15,18 @@ int Activities::school(){
 	
 	cout << "You decided to go to school";
 	player.addIntel(2);
+}
 
+int Activities::gym() {
+
+	Player player;
+	cout << "You decided to visit the gym!";
+	player.addStr(2);
+}
+
+int Activities::home() {
+	cout << "What do you want to do at home?";
+	cout << "1.Sleep";
 
 }
+
