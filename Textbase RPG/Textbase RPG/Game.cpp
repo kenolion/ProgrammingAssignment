5f54dcdb71610a23fba6476bbf291fe46ea5b41c
kenolion <veikeith@yahoo.com>
2016-08-21 19:25:01 +0800

    stuff

--- Textbase RPG/Textbase RPG/Game.cpp
+++ Textbase RPG/Textbase RPG/Game.cpp
@@ -30,121 +30,120 @@ int Game::battleSystem(Player *player, Monster monster, int monsterID) {
 		monster.damage = monster.attack(monsterID - 1);			//generated monsters damage each loop because monster will attack each round
 		ConsoleWindow::equalSignMaker9000(80);
 		do {												// THIS do while loop is to loop until player wants to exit from potion or skill menu condition at LINE 136
-		cout << "You encountered a" << monster.characterVector[monsterID - 1].characterName << endl;					//1. DISPLAY ENCOUNTER MESSAGE																							
-		cout << "What do you want to do?\n1.Attack\n2.NIGERO(Run)\n3.Potion\n4.Skills\n";
-		ConsoleWindow::equalSignMaker9000(80);
-																		
-		
-		while (!(cin >> choice)) {																						//2. GET USER INPUT
-			cout << "Incorrect input. Please try again.\n";
-			cin.clear();
-			cin.ignore(100, '\n');
-		}
-		switch (choice) {
-		case 1: {																									// ATTACK
+			cout << "You encountered a" << monster.characterVector[monsterID - 1].characterName << endl;					//1. DISPLAY ENCOUNTER MESSAGE																							
+			cout << "What do you want to do?\n1.Attack\n2.NIGERO(Run)\n3.Potion\n4.Skills\n";
+			ConsoleWindow::equalSignMaker9000(80);
 
-			player->damage = player->attack();
-			monster.characterVector[monsterID - 1].hp -= player->damage; //MINUSING THE MONSTERS HP
 
-			cout << "You did " << player->damage << " damage." << endl;
-			Sleep(1000);
-			break;
-		}
-		case 2: {
-			Sleep(1000);
-			cout << "You attempt to run away.\n";
-			int randNum;
-			randNum = rand() % 100 ; // 100 
-			if (randNum <= 30) //30% chance for the player to escapee
-			{
-				Sleep(1000);
-				cout << "You successfully managed to get away!\n";
-				Sleep(1000);
-				system("cls");
-
-				return 0;
-			}
-			else
-			{
-				Sleep(1000);
-
-				cout << "The enemy managed to catch up to you!\n";
-				break;
-
-			}
-		}
-		case 3: {
-			cout << "\nPick your potion to consume :\n\n";
-			ConsoleWindow::getCursorXY();
-			// EDITED BY KEITH
-			player->showPotions();
-			ConsoleWindow::SetDrawingPoint(0, ConsoleWindow::y+1);
-			cout << "-5. Cancel";
-			cout << "\n\nPotion choice (Number) : ";
-			do{
-			while (!(cin >> choice)) {
+			while (!(cin >> choice)) {																						//2. GET USER INPUT
 				cout << "Incorrect input. Please try again.\n";
 				cin.clear();
 				cin.ignore(100, '\n');
 			}
-			
-			 if (choice == -5) {
-				 system("cls");
+			switch (choice) {
+			case 1: {																									// ATTACK
+
+				player->damage = player->attack();
+				monster.characterVector[monsterID - 1].hp -= player->damage; //MINUSING THE MONSTERS HP
+
+				cout << "You did " << player->damage << " damage." << endl;
+				Sleep(1000);
 				break;
 			}
-			else if (player->getPotionQuantity(choice) == 0)
-			{
-				cout << "You're out of " << player->getPotionName(choice) << "s!\n\n";
-				cout << "Use another potion :";
-			}
-			else if(player->getPotionQuantity(choice)>0){
+			case 2: {
 				Sleep(1000);
-				player->addPotion(-1, choice);
-				cout << "You used :" << player->getPotionName(choice) << "!\n";
-				cout << "Player HP + " << player->getPotionHeal(choice) << "!\n\n";
-				player->setHp(player->getHp() + player->getPotionHeal(choice));
+				cout << "You attempt to run away.\n";
+				int randNum;
+				randNum = rand() % 100; // 100 
+				if (randNum <= 30) //30% chance for the player to escapee
+				{
+					Sleep(1000);
+					cout << "You successfully managed to get away!\n";
+					Sleep(1000);
+					system("cls");
+
+					return 0;
+				}
+				else
+				{
+					Sleep(1000);
+
+					cout << "The enemy managed to catch up to you!\n";
+					break;
+
+				}
+			}
+			case 3: {
+				cout << "\nPick your potion to consume :\n\n";
+				ConsoleWindow::getCursorXY();
+				// EDITED BY KEITH
+				player->showPotions();
+				ConsoleWindow::SetDrawingPoint(0, ConsoleWindow::y + 1);
+				cout << "-5. Cancel";
+				cout << "\n\nPotion choice (Number) : ";
+				do {
+					while (!(cin >> choice)) {
+						cout << "Incorrect input. Please try again.\n";
+						cin.clear();
+						cin.ignore(100, '\n');
+					}
+
+					if (choice == -5) {
+						system("cls");
+						break;
+					}
+					else if (player->getPotionQuantity(choice) == 0)
+					{
+						cout << "You're out of " << player->getPotionName(choice) << "s!\n\n";
+						cout << "Use another potion :";
+					}
+					else if (player->getPotionQuantity(choice) > 0) {
+						Sleep(1000);
+						player->addPotion(-1, choice);
+						cout << "You used :" << player->getPotionName(choice) << "!\n";
+						cout << "Player HP + " << player->getPotionHeal(choice) << "!\n\n";
+						player->setHp(player->getHp() + player->getPotionHeal(choice));
+					}
+
+				} while (player->getPotionQuantity(choice) == 0);
+				break;
+
 			}
 
-			} while (player->getPotionQuantity(choice) == 0);
-			break;
 
-		}
+			case 4: { ////////////////////////////////////////////SKILLS SYSTEM (SUPER INEFFICIENT)/////////////////////////////////////////////////////////////////// ZER MADE THIS CATASTROPHE(FIXED)
+				player->showSkills();
+				cout << endl << endl;
+				cout << "-5. Cancel\n";
+				cout << "Select your skill to use! : ";
+				while (!(cin >> choice)) {
+					cout << "Incorrect input. Please try again.\n";
+					cin.clear();
+					cin.ignore(100, '\n');
+				}
+				if (choice != -5) {
+					cout << "You CHANNEL " << player->getSkillCost(choice) << " mana !\n";
+					Sleep(1000);
+					cout << "You used " << player->getSkillName(choice) << "!";
+					monster.characterVector[monsterID - 1].hp -= player->getSkillDmg(choice);
+					cout << "\nYou did " << player->getSkillDmg(choice) << " damage." << endl;
 
-
-		case 4: { ////////////////////////////////////////////SKILLS SYSTEM (SUPER INEFFICIENT)/////////////////////////////////////////////////////////////////// ZER MADE THIS CATASTROPHE(FIXED)
-			player->showSkills();
-			cout << endl << endl;
-			cout << "-5. Cancel\n";
-			cout << "Select your skill to use! : ";
-			while (!(cin >> choice)) {
-				cout << "Incorrect input. Please try again.\n";
-				cin.clear();
-				cin.ignore(100, '\n');
-			}
-			if(choice !=-5){
-				cout << "You CHANNEL " <<player->getSkillCost(choice) << " mana !\n";
-				Sleep(1000);
-				cout << "You used " << player->getSkillName(choice) << "!";
-				monster.characterVector[monsterID - 1].hp -= player->getSkillDmg(choice);
-				cout << "\nYou did " << player->getSkillDmg(choice) << " damage." << endl;
-				
-				Sleep(1000);
+					Sleep(1000);
+					break;
+				}
 				system("cls");
-				break;
 			}
-			system("cls");
-		}
 
-		}
-		}while (choice == -5);								// value to not exit , sentinel value(?)forgot whats it called
-		if (monster.getHp(monsterID - 1) > 0){
-		player->setHp(player->getHp() - monster.damage); // Monster turn to attack
-		cout << "The monster did " << monster.damage << " damage" << endl;
-		system("pause");
+			}
+		} while (choice == -5);								// value to not exit , sentinel value(?)forgot whats it called
+		if (monster.getHp(monsterID - 1) > 0) {
+			player->setHp(player->getHp() - monster.damage); // Monster turn to attack
+			cout << "The monster did " << monster.damage << " damage" << endl;
+			system("pause");
 		}
 		ConsoleWindow::equalSignMaker9000(80);
-		system("cls");
-		
+
+
 		Time::calculateTime(6);
 	}										/// EXITING WHILE LOOP
 	if (player->getHp() <= 0) {
@@ -164,4 +163,4 @@ int Game::battleSystem(Player *player, Monster monster, int monsterID) {
 		system("cls");
 		return 0;
 	}
-
+}
